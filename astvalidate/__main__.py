6379daa1e5181b2d048275b2ef17b99383c1a091
Batuhan Taskaya <batuhanosmantaskaya@gmail.com>
2020-05-04 20:36:07 +0300

    enhance command line script

--- astvalidate/__main__.py
+++ astvalidate/__main__.py
@@ -1,23 +1,38 @@
 import ast
 import tokenize
 from argparse import ArgumentParser
+from pathlib import Path
 
 from astvalidate import validate
+
+
+def browse(paths):
+    for path in paths:
+        if path.is_file():
+            yield path
+        else:
+            yield from path.glob("**/*.py")
+
+
+def validate_single(path, level):
+    with tokenize.open(path) as f:
+        source = f.read()
+    tree = ast.parse(source)
+    return validate(tree, level=level)
 
 
 def main():
     parser = ArgumentParser(
         description="Validate the integrity of python files"
     )
-    parser.add_argument("file")
-    parser.add_argument("--level", type=int, default=None)
+    parser.add_argument("paths", nargs="+", type=Path)
+    parser.add_argument("-l", "--level", type=int, default=None)
 
     options = parser.parse_args()
-    with tokenize.open(options.file) as f:
-        source = f.read()
-    tree = ast.parse(source)
-    if validate(tree, level=options.level):
-        print("No problem found on given file!")
+    for path in browse(options.paths):
+        validate_single(path, options.level)
+    else:
+        print("No problems found!")
 
 
 if __name__ == "__main__":
